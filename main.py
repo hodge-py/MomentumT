322731428cb960b158a12b5c629610c4c6968c7c
hodge-py <105604814+hodge-py@users.noreply.github.com>
2025-12-29 09:34:13 -0700

    c

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ class MomentumT:
 
         for x in range(len(stocks)):
             try:
+                uptrend = False
                 ticker = stocks[x]
-                ticker_data = yf.Ticker(stocks[x]).history(period="6mo", interval="1d")
+                ticker_data = yf.Ticker(stocks[x]).history(period="1y", interval="1d")
                 ticker_data = ticker_data['Close']
                 month_days = ticker_data[-30:]
                 week_days = ticker_data[-7:]
@@ -25,7 +26,20 @@ class MomentumT:
                 weekly_momentum = ((week_days.iloc[-1] - week_days.iloc[0]) / week_days.iloc[0]) * 100
                 monthly_momentum = ((month_days.iloc[-1] - month_days.iloc[0]) / month_days.iloc[0]) * 100
                 quarterly_momentum = ((quarter_days.iloc[-1] - quarter_days.iloc[0]) / quarter_days.iloc[0]) * 100
-                container = [{'Ticker': ticker,'Sector': sector[x],'Category': category[x],'Weekly_Momentum': weekly_momentum,'Monthly_Momentum': monthly_momentum,'Quarterly_Momentum': quarterly_momentum }]
+
+                currentPrice = ticker_data.iloc[-21]
+                start_price = ticker_data.iloc[0]
+
+                momentum_score = (currentPrice - start_price) / start_price * 100
+
+                moving_200 = ticker_data.rolling(window=200).mean()
+                current_price = ticker_data.iloc[-1]
+
+                if momentum_score > 0.10 and current_price > moving_200.iloc[-1]:
+                    uptrend = True
+
+
+                container = [{'Ticker': ticker,'Sector': sector[x],'Category': category[x],'Weekly_Momentum': weekly_momentum,'Monthly_Momentum': monthly_momentum,'Quarterly_Momentum': quarterly_momentum, 'Momentum_Score': momentum_score, 'Uptrend': uptrend}]
 
                 stockFrame = pd.concat([stockFrame, pd.DataFrame(container)], ignore_index=True)
 
